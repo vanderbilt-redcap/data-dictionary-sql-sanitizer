1c5245cf403a0e2dbf7e70dc99efdba903aff51f
bascome <eva.bascompte.moragas@vumc.org>
2026-02-11 12:49:04 -0600

    added quotes if blank

--- DataDictionarySQLSanitizer.php
+++ DataDictionarySQLSanitizer.php
@@ -199,7 +199,11 @@ class DataDictionarySQLSanitizer extends AbstractExternalModule
                         foreach ($sqlEntry['sqlVariables'] as $sqlKey => $sqlValue) {
                             if ($sqlKey == $row['field_name']) {
                                 // Replacement logic
-                                $replacement = $sqlEntry['constant'];
+                                if(empty($sqlEntry['constant'])){
+                                    $replacement = "''";
+                                }else{
+                                    $replacement = $sqlEntry['constant'];
+                                }
                                 $row['select_choices_or_calculations'] = preg_replace_callback(
                                     $patterns,
                                     function ($matches) use ($sqlEntry, $replacement) {
